2330a06f2273917e11a6b9113fda1bc6201b2e7f
Tom Battaglia <battagliat@gmail.com>
2013-07-17 21:57:22 -0600

    completed user functionality and began application work.

---
+++ src/edu/umt/exceptions/ApplicationCreationException.java
@@ -0,0 +1,11 @@
+package edu.umt.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tb189431e
+ * Date: 7/17/13
+ * Time: 8:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationCreationException {
+}
---
+++ src/edu/umt/jsf/managedbeans/ApplicationBackingBean.java
@@ -0,0 +1,11 @@
+package edu.umt.jsf.managedbeans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tb189431e
+ * Date: 7/17/13
+ * Time: 7:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationBackingBean {
+}
